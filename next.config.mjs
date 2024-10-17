af33acb49b4a93a4093e5bd7bacefb775b13ec62
Sanusi Hassan <sanusihassan122@gmail.com>
2024-10-18 00:23:27 +0300

    packaged assets

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/test-route" : "",
+  assetPrefix: isProd ? "/test-route-assets" : "",
   transpilePackages: ["pdfequips-navbar"],
   output: "export"
 };
